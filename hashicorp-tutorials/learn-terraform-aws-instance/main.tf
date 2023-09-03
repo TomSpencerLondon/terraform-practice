99e97c6c3bb40cf786de03fc60a12dbcd7fe86c5
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2023-09-03 19:23:12 +0100

    add terraform practice

--- hashicorp-tutorials/learn-terraform-aws-instance/main.tf
+++ hashicorp-tutorials/learn-terraform-aws-instance/main.tf
@@ -1,4 +1,11 @@
 terraform {
+ cloud {
+    organization = "tom-spencer"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
